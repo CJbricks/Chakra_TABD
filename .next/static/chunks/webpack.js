9dd0e9652c68e0ff560f935f227ceab2ce7441bb
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-04-14 16:39:01 -0600

    Added Test component to practice fetch

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba325f8cbb4dbf90"; }
+/******/ 		__webpack_require__.h = function() { return "d2ba8c7bcbacbcaa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f149abb9e047bf4c.hot-update.js
+++ .next/static/webpack/webpack.f149abb9e047bf4c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2e78a7d47cb19f4b"; }
+/******/ 	__webpack_require__.h = function() { return "d2ba8c7bcbacbcaa"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
